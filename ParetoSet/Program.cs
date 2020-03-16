87a0d04f90ce09b78c33acada10d11456b754975
Koptsev G.A <g.koptsev@abonentplus.ru>
2020-03-16 08:20:30 +0300

    Initial commit

---
+++ ParetoSet/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParetoSet
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            //Ввод количества критериев
+            var critCnt = FillCritCnt();
+            //Ввод количества векторов
+            var vectorCnt = FillCaseCnt();
+            //Ввод значений векторов
+            var matrix = FillMatrix(critCnt, vectorCnt);
+            //Реализация алгоритма
+            var paretoSet = GetParetoSet(matrix);
+            //Вывод результата
+            Console.WriteLine("Результат: ");
+            for (int i = 0; i < paretoSet.Count; i++)
+            {
+                Console.Write($"Вектор {i + 1}: (");
+                Console.Write(string.Join(", ", paretoSet[i]));
+                Console.WriteLine(")");
+            }
+            Console.WriteLine("Завершение работы программы, нажмите любую клавишу...");
+            Console.ReadLine();
+        }
+
+        private static int FillCritCnt()
+        {
+        critCnt:
+            Console.WriteLine("Введите количество критериев");
+            if (int.TryParse(Console.ReadLine(), out int result))
+            {
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод");
+                goto critCnt;
+            }
+        }
+
+        private static int FillCaseCnt()
+        {
+        caseCnt:
+            Console.WriteLine("Введите количество векторов");
+            if (int.TryParse(Console.ReadLine(), out int result))
+            {
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод");
+                goto caseCnt;
+            }
+        }
+
+        private static List<List<int>> FillMatrix(int critCnt, int vectorCnt)
+        {
+            var resultSet = new List<List<int>>();
+            for (int i = 0; i < vectorCnt; i++)
+            {
+                var currentVector = new List<int>();
+                Console.WriteLine($"Заполните вектор {i + 1} (через Enter)");
+                for (int j = 0; j < critCnt; j++)
+                {
+                error:
+                    Console.WriteLine($"Заполните критерий {j + 1}");
+                    if (int.TryParse(Console.ReadLine(), out int result))
+                    {
+                        currentVector.Add(result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Надо ввести число");
+                        goto error;
+                    }
+                }
+                resultSet.Add(currentVector);
+            }
+            return resultSet;
+        }
+
+        private static List<List<int>> GetParetoSet(List<List<int>> matrix)
+        {
+            var paretoSet = new List<List<int>>();
+            matrix.ForEach(v => paretoSet.Add(v));
+            int i = 0;
+            int j = 1;
+
+        shag2:
+            if (j == matrix.Count) return paretoSet;
+            if (GetSumOfVector(matrix[i]) > GetSumOfVector(matrix[j]))
+            {
+                paretoSet.Remove(matrix[j]);
+                if (j < matrix.Count)
+                {
+                    j++;
+                    goto shag2;
+                }
+                else
+                {
+                    goto shag7;
+                }
+            }
+            else
+            {
+                if (j == matrix.Count) return paretoSet;
+                if (GetSumOfVector(matrix[j]) > GetSumOfVector(matrix[i]))
+                {
+                    paretoSet.Remove(matrix[i]);
+                    goto shag7;
+                }
+                else
+                {
+                    if (j < matrix.Count)
+                    {
+                        j++;
+                        goto shag2;
+                    }
+                    else
+                    {
+                        goto shag7;
+                    }
+                }
+            }
+
+        shag7:
+            if (i < matrix.Count - 1)
+            {
+                i++;
+                j = i + 1;
+                goto shag2;
+            }
+            else
+            {
+                return paretoSet;
+            }
+
+        }
+
+        private static int GetSumOfVector(List<int> vector)
+        {
+            int sum = 0;
+            foreach (var element in vector)
+                sum += element;
+            return sum;
+        }
+    }
+}
